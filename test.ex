63d7bd1ea52b167769a12faf9ff7353c2ff7ec80
dronabanerjee <drona95.db@gmail.com>
2019-09-04 05:01:38 +0530

    Calculating Vampire Numbers with Permutation(with redundancy)

--- test.ex
+++ test.ex
@@ -1,39 +1,51 @@
 defmodule M do
   def hello do
-     #IO.puts "Chicken is my religion"
-     #spawn(fn() -> loop(20, 10) end)
-     #spawn(fn() -> loop(9, 0) end)
-     n = 1260
-     a = rem(n,10)
-     t1 = div(n,10)
-     b = rem(t1,10)
-     t2 = div(n, 100)
-     c = rem(t2, 10)
-     t3 = div(n, 1000)
-     d = rem(t3,10)
 
-     #permutations([a,b,c,d])
+     n1 = IO.gets("Enter Starting Number: ")    |> String.strip |> String.to_integer
+     n2 = IO.gets("Enter Ending Number: ")    |> String.strip |> String.to_integer
 
-     Enum.each(permutations([a,b,c,d]), fn(x) ->
-       IO.puts String.to_integer(Enum.join(x, ""))
+     range = n2-n1
+     interval = div(range, 5)
 
-     end)
-     #IO.puts "Perm List is : #{perm_list}"
+     spawn(fn() -> loop(n1, n1+interval) end)
+     spawn(fn() -> loop(n1+interval+1, n1+interval*2) end)
+     spawn(fn() -> loop(n1+interval*2+1, n1+interval*3) end)
+     spawn(fn() -> loop(n1+interval*3+1, n1+interval*4) end)
+     spawn(fn() -> loop(n1+interval*4+1, n2) end)
 
   end
 
+  def pow(_, 0), do: 1
+  def pow(k, n) when rem(n,2) == 1, do: k * pow(k, n - 1)
+  def pow(k, n) do
+  result = pow(k, div(n, 2))
+  result * result
+  end
+
+
+  def loop(0,_), do: nil
+
+  def loop(min, max) do
+    if max < min do
+      loop(0, min)
+    else
+      if rem(length(Integer.digits(min)), 2) == 0 do
+        #IO.puts "Num is #{min} and length is #{length(Integer.digits(min))}"
+        Enum.each(permutations(Integer.digits(min)), fn(x) ->
+
+          if div(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2)))*rem(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2))) == min do
+
+              IO.puts "#{min} " <> Integer.to_string(div(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2)))) <> " " <> Integer.to_string(rem(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2))))
+
+          end
+        end)
+
+      end
+      loop(min+1, max)
+    end
+  end
 
   def permutations([]), do: [[]]
   def permutations(list), do: for elem <- list, rest <- permutations(list--[elem]), do: [elem|rest]
 
-  def loop(0,_), do: nil
-
-  def loop(max, min) do
-    if max < min do
-      loop(0, min)
-    else
-      IO.puts "Num is #{max}"
-      loop(max - 1, min)
-    end
-  end
 end
