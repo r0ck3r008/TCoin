cf43c337d8697c1dfc564956af941034b17576b6
dronabanerjee <drona95.db@gmail.com>
2019-09-05 03:25:18 +0530

    Vampire numbers in the required format with multiple pair of fangs using permutation algo

--- test.ex
+++ test.ex
@@ -1,5 +1,12 @@
 defmodule M do
+
+  def start_link do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
   def hello do
+
+     M.start_link
 
      n1 = IO.gets("Enter Starting Number: ")    |> String.strip |> String.to_integer
      n2 = IO.gets("Enter Ending Number: ")    |> String.strip |> String.to_integer
@@ -12,6 +19,9 @@ defmodule M do
      spawn(fn() -> loop(n1+interval*2+1, n1+interval*3) end)
      spawn(fn() -> loop(n1+interval*3+1, n1+interval*4) end)
      spawn(fn() -> loop(n1+interval*4+1, n2) end)
+
+     v = Agent.get(__MODULE__, fn(state) -> state end)
+     Enum.each(v, fn ({key, value}) -> IO.puts "#{key} : #{Enum.join(value, " ")}" end)
 
   end
 
@@ -26,6 +36,7 @@ defmodule M do
   def loop(0,_), do: nil
 
   def loop(min, max) do
+
     if max < min do
       loop(0, min)
     else
@@ -35,7 +46,22 @@ defmodule M do
 
           if div(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2)))*rem(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2))) == min do
 
-              IO.puts "#{min} " <> Integer.to_string(div(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2)))) <> " " <> Integer.to_string(rem(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2))))
+              #IO.puts "#{min} " <> Integer.to_string(div(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2)))) <> " " <> Integer.to_string(rem(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2))))
+
+              Agent.update(__MODULE__, fn(state) ->
+
+              Map.update(state, :"#{min}", [div(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2))), rem(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2)))], &(
+
+              if !Enum.member?(Map.get(state, :"#{min}"), rem(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2)))) do
+                &1 ++ [div(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2))), rem(String.to_integer(Enum.join(x, "")), pow(10,div(length(Integer.digits(min)),2)))]
+              else
+                [1 | &1] -- [1]
+              end
+
+              )
+              )
+
+              end)
 
           end
         end)
