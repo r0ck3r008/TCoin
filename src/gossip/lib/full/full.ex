6f0ac1518789391c3559b9bb11e10cd1ac323bf8
r0ck3r008 <naman.arora@tutanota.com>
2019-09-24 20:07:44 -0400

    fixed a typo bug where num was addressed as n also since worker is a tuple, fixed its access method

--- src/gossip/lib/full/full.ex
+++ src/gossip/lib/full/full.ex
@@ -5,13 +5,13 @@ defmodule Full do
     {:ok, agnt_pid}=Agent.start_link(fn->[] end)
 
     #spawn workers
-    workers=for _x<-0..n-1, do: Full.Worker.start_link
+    workers=for _x<-0..num-1, do: Full.Worker.start_link
 
     #update agent
     Agent.update(agnt_pid, &(&1++workers))
 
     #send msgs to all workers that neighbours are ready to be fetched
-    for worker<-workers, do: Full.Worker.update_nbor_state(worker, agnt_pid)
+    for worker<-workers, do: Full.Worker.update_nbor_state(elem(worker, 1), agnt_pid)
   end
 
 end
