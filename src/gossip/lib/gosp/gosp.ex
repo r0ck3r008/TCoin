f315c32b654eee13e20f5c773fe31e96b8c76f23
r0ck3r008 <naman.arora@tutanota.com>
2019-09-30 22:08:30 -0400

    Salty -1 has been removed due to its range

--- src/gossip/lib/gosp/gosp.ex
+++ src/gossip/lib/gosp/gosp.ex
@@ -5,7 +5,7 @@ defmodule Gosp do
     [_|nbors]=wrkr_mod.get_nbors(from)
 
     #broadcast select rand neighbour
-    rand_num=Salty.Random.uniform(length(nbors))
+    rand_num=Salty.Random.uniform(length(nbors)-1)
     nbor=Enum.at(nbors, rand_num)
     recv_rum(wrkr_mod, nbor)
   end
@@ -13,12 +13,16 @@ defmodule Gosp do
   #called inside the receiver's public API
   def recv_rum(wrkr_mod, to) do
     n_rounds=wrkr_mod.get_round(to)
-    if n_rounds==9 do
-      wrkr_mod.converge(to)
-    else
-      wrkr_mod.inc_round(to)
-      #send back out again
-      send_rum(wrkr_mod, to)
+    #    IO.puts ("#{n_rounds}")
+    case n_rounds do
+      9->
+        wrkr_mod.converge(to)
+      10->
+        :ok
+      _->
+        wrkr_mod.inc_round(to)
+        #send back out again
+        send_rum(wrkr_mod, to)
     end
   end
 
