de6f2c74e14dd2420c0b57a33168725f72e773dd
r0ck3r008 <naman.arora@tutanota.com>
2019-10-01 03:30:00 -0400

    made torus to work with gossip, timer still doesnt work

--- src/gossip/lib/torus/torus.ex
+++ src/gossip/lib/torus/torus.ex
@@ -1,6 +1,9 @@
 defmodule Torus do
 
-  def main(n3) do
+  use GenServer
+
+  #external API
+  def start_link(n3) do
     n=ceil(:math.pow(n3, :math.pow(3, -1)))
     chk_cube_rt(n3, n)
 
@@ -10,11 +13,76 @@ defmodule Torus do
     #start dispenser
     {:ok, disp_pid}=Torus.Dispenser.start_link
 
+    #start timer
+    {:ok, timer_pid}=Timer.start_link
+
+    #start server
+    {:ok, main_pid}=GenServer.start_link(__MODULE__, {n3, timer_pid})
+
     #Fork workers
-    for _x<-0..n3-1, do: Torus.Worker.start_link(n, agnt_pid, disp_pid)
+    workers=for _x<-0..n3-1, do: Torus.Worker.start_link
+    workers=for {_, wrkr}<-workers, do: wrkr
+    tasks=for wrkr<-workers, do: Task.async(fn-> Torus.Worker.update_state(wrkr, n, agnt_pid, disp_pid, main_pid) end)
+
+    for task<-tasks, do: Task.await(task, :infinity)
+
+    #start rumor
+    start_rumor(n, agnt_pid, timer_pid, disp_pid)
   end
 
   def chk_cube_rt(n3, n) when rem(n3, n)==1, do: System.halt(1)
   def chk_cube_rt(n3, n) when rem(n3, n)==0, do: :ok
+
+  def start_rumor(num, agnt_pid, timer_pid, disp_pid) do
+    #deadlock protection
+    n3=ceil(:math.pow(num, 3))
+    Torus.Worker.remove_deadlocks(disp_pid, n3, n3-Torus.Dispenser.get_done_count(disp_pid))
+    #start timer
+    Timer.start_timer(timer_pid)
+
+    #send first rumor
+    wrkr_mod=Torus.Worker
+    Gosp.send_rum(
+      wrkr_mod,
+      Agent.get(agnt_pid, &Map.get(&1,{
+        Salty.Random.uniform(num),
+        Salty.Random.uniform(num),
+        Salty.Random.uniform(num)
+      }
+      ))
+    )
+  end
+
+  def get_state(self_pid) do
+    GenServer.call(self_pid, :get_state)
+  end
+
+  def converged(self_pid) do
+    {n3, n_converged, timer_pid}=get_state(self_pid)
+    delta=n3-n_converged
+
+    if delta==1 do
+      Timer.end_timer(timer_pid)
+      GenServer.stop(self_pid, :normal)
+    else
+      GenServer.cast(self_pid, :inc_converged)
+    end
+  end
+
+  #callbacks
+  @impl true
+  def init(attrs) do
+    {:ok, {elem(attrs, 0), 0, elem(attrs, 1)}}
+  end
+
+  @impl true
+  def handle_cast(:inc_converged, state) do
+    {:noreply, {elem(state, 0), elem(state, 1)+1, elem(state, 2)}}
+  end
+
+  @impl true
+  def handle_call(:get_state, _from, state) do
+    {:reply, state, state}
+  end
 
 end
--- src/gossip/lib/torus/worker.ex
+++ src/gossip/lib/torus/worker.ex
@@ -3,9 +3,13 @@ defmodule Torus.Worker do
   use GenServer
 
   #public API
-  def start_link(num, agnt_pid, disp_pid) do
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok)
+  end
+
+  def update_state(self_pid, num, agnt_pid, disp_pid, main_pid) do
     #find co_ordinates
-    co_ords=fetch_co_ords(num, agnt_pid, disp_pid, nil)
+    co_ords=fetch_co_ords(num, self_pid, agnt_pid, disp_pid, nil)
 
     #fetch neighbors
     nbor_co_ords=calc_nbor_co_ords(
@@ -22,34 +26,41 @@ defmodule Torus.Worker do
       [],
       0
     )
-    nbor_dir=fetch_nbors(nbor_co_ords, agnt_pid, %{co_ords=>self()}, 0)
-
-    GenServer.start_link(__MODULE__, nbor_dir)
+    n3=ceil(:math.pow(num, 3))
+    remove_deadlocks(disp_pid, n3, n3-Torus.Dispenser.get_done_count(disp_pid))
+    nbor_dir=fetch_nbors(nbor_co_ords, agnt_pid, [main_pid], 0)
+    GenServer.cast(self_pid, {:update_state, nbor_dir})
   end
 
-  def fetch_co_ords(num, agnt_pid, disp_pid, nil) do
+  def fetch_co_ords(num, self_pid, agnt_pid, disp_pid, nil) do
     fetch_co_ords(num,
+      self_pid,
       agnt_pid,
       disp_pid,
       Torus.Dispenser.chk_co_cord(disp_pid,
-        {:rand.uniform(num)-1, :rand.uniform(num-1)-1, :rand.uniform(num-1)-1},
-        agnt_pid, self()))
+        {Salty.Random.uniform(num), Salty.Random.uniform(num), Salty.Random.uniform(num)},
+        agnt_pid, self_pid))
   end
-  def fetch_co_ords(_num, _agnt_pid, _disp_pid, co_ords), do: co_ords
+  def fetch_co_ords(_num, _self_pid,  _agnt_pid, _disp_pid, co_ords), do: co_ords
 
   def calc_nbor_co_ords(_num, _co_ords, _dlta_mat, nbor_co_ords, 6), do: nbor_co_ords
   def calc_nbor_co_ords(num, co_ords, dlta_mat, nbor_co_ords, count) do
-    calc_nbor_co_ords(num, co_ords, dlta_mat,
-      nbor_co_ords++[{
-        calc_mirror(num, elem(co_ords, 0)+elem(Enum.at(dlta_mat, 0), count)),
-        calc_mirror(num, elem(co_ords, 0)+elem(Enum.at(dlta_mat, 0), count)),
-        calc_mirror(num, elem(co_ords, 0)+elem(Enum.at(dlta_mat, 0), count)),
-      }], count+1)
+    new_nbor_co_ords=nbor_co_ords++[{
+        calc_mirror(num, elem(co_ords, 0)+elem(Enum.at(dlta_mat, count), 0)),
+        calc_mirror(num, elem(co_ords, 1)+elem(Enum.at(dlta_mat, count), 1)),
+        calc_mirror(num, elem(co_ords, 2)+elem(Enum.at(dlta_mat, count), 2)),
+      }]
+    calc_nbor_co_ords(num, co_ords, dlta_mat, new_nbor_co_ords, count+1)
   end
 
   def calc_mirror(num, n) when n<0, do: num-1
   def calc_mirror(num, n) when n>num-1, do: 0
   def calc_mirror(_num, n), do: n
+
+  def remove_deadlocks(_disp_pid, _num, 0), do: :ok
+  def remove_deadlocks(disp_pid, num, _dlta) do
+    remove_deadlocks(disp_pid, num, num-Torus.Dispenser.get_done_count(disp_pid))
+  end
 
   def fetch_nbors(_nbor_co_ords, _agnt_pid, nbor_dir, 6), do: nbor_dir
   def fetch_nbors(nbor_co_ords,
@@ -62,16 +73,53 @@ defmodule Torus.Worker do
         fetch_nbors(nbor_co_ords, agnt_pid, nbor_dir, count)
       _->
         fetch_nbors(nbor_co_ords, agnt_pid,
-          Map.put(nbor_dir, Enum.at(nbor_co_ords, count), nbor_pid),
+          nbor_dir++[nbor_pid],
           count+1
         )
     end
   end
 
+  def inc_round(of) do
+    GenServer.cast(of, :inc_round)
+  end
+
+  def get_round(of) do
+    GenServer.call(of, :get_round)
+  end
+
+  def get_nbors(of) do
+    GenServer.call(of, :get_nbors)
+  end
+
+  def converge(of) do
+    [main_pid|_]=get_nbors(of)
+    Torus.converged(main_pid)
+  end
+
   #callbacks
   @impl true
-  def init(nbor_dir) do
-    {:ok, nbor_dir}
+  def init(:ok) do
+    {:ok, []}
+  end
+
+  @impl true
+  def handle_cast({:update_state, state}, _) do
+    {:noreply, {state, 0}}
+  end
+
+  @impl true
+  def handle_cast(:inc_round, state) do
+    {:noreply, {elem(state, 0), elem(state , 1)+1}}
+  end
+
+  @impl true
+  def handle_call(:get_nbors, _from, state) do
+    {:reply, elem(state, 0), state}
+  end
+
+  @impl true
+  def handle_call(:get_round, _from, state) do
+    {:reply, elem(state, 1), state}
   end
 
 end
