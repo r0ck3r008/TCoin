c6a33120f37eee70ba620e5caecbf67d2e99d6cd
r0ck3r008 <naman.arora@tutanota.com>
2019-10-02 19:45:56 -0400

    made testing for full

--- src/gossip/test/full_test.exs
+++ src/gossip/test/full_test.exs
@@ -1,8 +1,12 @@
 defmodule Full.Test do
   use ExUnit.Case
 
-  test "Full topology" do
-    assert Gossip.start(1000, Full)
+  test "Full topology with PushSum Algorithm" do
+    assert Gossip.start(1000, Full, PushSum)
+  end
+
+  test "Full topology With Gossip Algorithm" do
+    assert Gossip.start(1000, Full, Gosp)
   end
 
 end
