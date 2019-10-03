9e5ae7dd6afed7dd1571dcef5bd6013d6322c496
r0ck3r008 <naman.arora@tutanota.com>
2019-10-02 20:54:35 -0400

    tests accompanying honeycomb rand

--- src/gossip/test/honeycomb_rand_test.exs
+++ src/gossip/test/honeycomb_rand_test.exs
@@ -1,8 +1,12 @@
 defmodule Honeycomb_rand.Test do
   use ExUnit.Case
 
-  test "Honeycomb Radom topology" do
-    assert Gossip.start(24, Honeycomb_rand)
+  test "Honeycomb Radom topology with PushSum algo" do
+    assert Gossip.start(24, Honeycomb_rand, PushSum)
+  end
+
+  test "Honeycomb Radom topology with Gossip" do
+    assert Gossip.start(24, Honeycomb_rand, Gosp)
   end
 
 end
