6a8d28fe70b62c6f695d989f4b23ac875d25656f
Drona Banerjee <drona95.db@gmail.com>
2019-10-03 02:45:39 +0000

    Added comments and removed debug statements

--- src/pushsum/rand2DPSWorker.ex
+++ src/pushsum/rand2DPSWorker.ex
@@ -15,18 +15,17 @@ defmodule Proj2.Rand2DPS.Worker do
     w_new = w + w_rec
     s_new = s + s_rec
     curr_sum_est = s_new / w_new
-    #w_send = (w + w_rec)/2
-    #s_send = (s + s_rec)/2
-    #curr_sum_est = s_send / w_send
     change = abs(curr_sum_est - sum_est)
     threshold = :math.pow(10, -10)
-
+    
+    #flagging worker as dead once the change in sum estimate is less than threshold for the third time
     flag =
     if(counter == 2 && change < threshold) do
       #Proj2.Rand2DPS.term(self())
       1
     end
 
+    #Worker stops propagating messages once flag value is set as 1
     counter =
     if(counter == 2 && change < threshold) do
       Proj2.Rand2DPS.term(self())
@@ -35,14 +34,17 @@ defmodule Proj2.Rand2DPS.Worker do
       if (counter < 3 && flag ==0) do
         nbor_pids = Proj2.Rand2DPS.get_nbors(self())
         next_pid = Enum.random(nbor_pids)
+        #preparing message payload to be sent to random neighbour
         w_send = w_new/2
         s_send = s_new/2
         GenServer.cast(next_pid, {:next, s_send, w_send})
+        #updating s and w values of the current node
         w_new = w_send
         s_new = s_send
         #curr_sum_est = s_new/w_new
       end
 
+      #resetting the counter to 0 if we find change is greater than threshold else counter keeps increasing
       if(change > threshold && flag ==0) do
         0
       else
