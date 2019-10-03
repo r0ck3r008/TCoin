1731a4ede932a61b1313ca54bcdde6cca025c51a
r0ck3r008 <naman.arora@tutanota.com>
2019-10-02 20:45:48 -0400

    accompanying tests

--- src/gossip/test/honeycomb_test.exs
+++ src/gossip/test/honeycomb_test.exs
@@ -1,8 +1,12 @@
 defmodule Honeycomb.Test do
   use ExUnit.Case
 
-  test "Honeycomb topology" do
-    assert Gossip.start(24, Honeycomb)
+  test "Honeycomb topology with PushSum algo" do
+    assert Gossip.start(24, Honeycomb, PushSum)
+  end
+
+  test "Honeycomb topology with Gossip Algo" do
+    assert Gossip.start(24, Honeycomb, Gosp)
   end
 
 end
