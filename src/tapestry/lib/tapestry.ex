7d000a3f53b638b9b914cd1e5d05b3e8dd262e5d
r0ck3r008 <naman.arora@tutanota.com>
2019-10-10 06:42:57 -0400

    new mix project dir structure

---
+++ src/tapestry/lib/tapestry.ex
@@ -0,0 +1,18 @@
+defmodule Tapestry do
+  @moduledoc """
+  Documentation for Tapestry.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Tapestry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
