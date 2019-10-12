1d2ed460b8357a9c6fe635ea05b1a332ff682f5a
r0ck3r008 <naman.arora@tutanota.com>
2019-10-12 02:40:42 -0400

    helper module to node

---
+++ src/tapestry/lib/node/helper.ex
@@ -0,0 +1,6 @@
+defmodule Tapestry.Node.Helper do
+
+  def remove_deadlocks(_num, _disp_pid, 0), do: :ok
+  def remove_deadlocks(num, disp_pid, dlta), do: remove_deadlocks(num, disp_pid, num-Tapestry.Dispenser.fetch_assigned(disp_pid))
+
+end
