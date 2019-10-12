abb92b034b4dc1196d40d191848963c48684d495
r0ck3r008 <naman.arora@tutanota.com>
2019-10-12 03:06:14 -0400

    node Genserver is not long running process, fixed the Task exit issue

--- src/tapestry/lib/init/init.ex
+++ src/tapestry/lib/init/init.ex
@@ -6,9 +6,18 @@ defmodule Tapestry.Init do
 
     #start nodes
     nodes=for _x<-0..n-1, do: Tapestry.Node.start_link
-    tasks=for {_, pid}<-nodes, do: Task.async(fn-> Tapestry.Node.update_route(pid, n, disp_pid) end)
+    tasks=for {_, pid}<-nodes, do: Task.async(fn-> task_fn(pid, n, disp_pid, 0) end)
     #makes main never exit
     for task<-tasks, do: Task.await(task, :infinity)
+  end
+
+  def task_fn(pid, n, disp_pid, 0) do
+    Tapestry.Node.update_route(pid, n, disp_pid)
+    task_fn(pid, n, disp_pid, 1)
+  end
+  def task_fn(pid, n, disp_pid, count) do
+    :timer.sleep(1000)
+    task_fn(pid, n, disp_pid, count)
   end
 
 end
