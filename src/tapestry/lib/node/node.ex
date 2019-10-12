5101ce1db5b00a2713f45927ca30930072b63dbc
r0ck3r008 <naman.arora@tutanota.com>
2019-10-12 02:40:11 -0400

    the node module as genserver

---
+++ src/tapestry/lib/node/node.ex
@@ -0,0 +1,34 @@
+defmodule Tapestry.Node do
+
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok)
+  end
+
+  def update_route(of, num, disp_pid) do
+    GenServer.cast(of, {:assign_hash, Salty.Random.uniform(10000000)})
+
+    #remove deadlocks
+    Tapestry.Node.Helper.remove_deadlocks(num, disp_pid, num-Tapestry.Dispenser.fetch_assigned(disp_pid))
+  end
+
+  #callbacks
+  @impl true
+  def init(:ok) do
+    {:ok, {}}
+  end
+
+  @impl true
+  def handle_cast({:assign_hash, hash}, disp_pid) do
+    {:noreply,
+      {Tapestry.Dispenser.assign_hash(disp_pid, self(), hash), disp_pid}
+    }
+  end
+
+  @impl true
+  def terminate(_, _) do
+    IO.puts "Terminating Node"
+  end
+
+end
