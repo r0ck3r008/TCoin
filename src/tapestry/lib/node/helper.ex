518b704f6336dca9a994c53c4a205dde2f2945b1
r0ck3r008 <naman.arora@tutanota.com>
2019-10-15 16:10:30 -0400

    fixed a compilation warning

--- src/tapestry/lib/node/helper.ex
+++ src/tapestry/lib/node/helper.ex
@@ -1,6 +1,6 @@
 defmodule Tapestry.Node.Helper do
 
   def remove_deadlocks(_num, _disp_pid, 0), do: :ok
-  def remove_deadlocks(num, disp_pid, dlta), do: remove_deadlocks(num, disp_pid, num-Tapestry.Dispenser.fetch_assigned(disp_pid))
+  def remove_deadlocks(num, disp_pid, _dlta), do: remove_deadlocks(num, disp_pid, num-Tapestry.Dispenser.fetch_assigned(disp_pid))
 
 end
