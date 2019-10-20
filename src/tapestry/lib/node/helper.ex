6ea66b31203488b83849ac209e12bdcab6e0862c
r0ck3r008 <naman.arora@tutanota.com>
2019-10-20 16:42:32 -0400

    started to implement publish protocol

--- src/tapestry/lib/node/helper.ex
+++ src/tapestry/lib/node/helper.ex
@@ -1,6 +1,27 @@
 defmodule Tapestry.Node.Helper do
 
+  def hash_it(msg) do
+    Salty.Hash.Sha256.hash(msg)
+    |> elem(1)
+    |> Base.encode16()
+    |> String.slice(0, 16)
+  end
+
   def remove_deadlocks(_num, _disp_pid, 0), do: :ok
   def remove_deadlocks(num, disp_pid, _dlta), do: remove_deadlocks(num, disp_pid, num-Tapestry.Dispenser.fetch_assigned(disp_pid))
+
+  def publish(nbors, agnt_pid, {hash, msg}) do
+    nbor=find_best_match(nbors, hash)
+    if elem(nbor, 1)==self() do
+      IO.puts "Publishing #{msg}:#{hash} at #{inspect self()}:#{elem(hd(nbors), 0)}"
+      Agent.update(agnt_pid, &(&1++[{hash, msg}]))
+    else
+      send(elem(nbor, 1), {:publish, msg})
+    end
+  end
+
+  def find_best_match([{self_hash, self_pid} | rest], hash) do
+    #RE match here
+  end
 
 end
--- src/tapestry/lib/node/node.ex
+++ src/tapestry/lib/node/node.ex
@@ -3,44 +3,58 @@ defmodule Tapestry.Node do
   use GenServer
 
   def start_link do
-    GenServer.start_link(__MODULE__, :ok)
+    {:ok, agnt_pid}=Agent.start_link(fn-> [] end)
+    GenServer.start_link(__MODULE__, agnt_pid)
   end
 
   def update_route(of, num, disp_pid) do
-    hash=String.slice(Base.encode16(elem(Salty.Hash.Sha256.hash(inspect of), 1)), 0, 16)
+    hash=Tapestry.Node.Helper.hash_it(inspect of)
     GenServer.cast(of, {:assign_hash, hash, disp_pid})
 
     #remove deadlocks
     Tapestry.Node.Helper.remove_deadlocks(num, disp_pid, num-Tapestry.Dispenser.fetch_assigned(disp_pid))
 
-    nbors=Tapestry.Dispenser.Hash_helper.get_nbors(disp_pid, hash)
-    GenServer.cast(of, {:update_nbors, nbors})
+    GenServer.cast(of,
+      {
+        :update_nbors,
+        Tapestry.Dispenser.Hash_helper.get_nbors(disp_pid, hash)
+      })
   end
 
   #callbacks
   @impl true
-  def init(:ok) do
-    {:ok, {}}
+  def init(agnt_pid) do
+    {:ok, agnt_pid}
   end
 
   @impl true
-  def handle_cast({:assign_hash, hash, disp_pid}, _state) do
+  def handle_cast({:assign_hash, hash, disp_pid}, agnt_pid) do
     {:noreply,
-      Tapestry.Dispenser.assign_hash(disp_pid, self(), hash)
+      {
+        Tapestry.Dispenser.assign_hash(disp_pid, self(), hash),
+        agnt_pid    
+      }
     }
   end
 
   @impl true
-  def handle_cast({:update_nbors, nbors}, hash) do
+  def handle_cast({:update_nbors, nbors}, {hash, agnt_pid}) do
     #remove self
     nbors=Enum.uniq([{hash, self()}]++nbors)
     IO.inspect nbors
-    {:noreply, {nbors, hash}}
+    {:noreply, {nbors, agnt_pid}}
   end
 
   @impl true
-  def terminate(_, _) do
-    IO.puts "Terminating Node"
+  def handle_info({:publish, msg}, {nbors, agnt_pid}) do
+    IO.puts "[#{elem(hd(nbors), 0)}] Publishing #{msg}!"
+    Tapestry.Node.Helper.publish(nbors, agnt_pid, {Tapestry.Node.Helper.hash_it(msg), msg})
+    {:noreply, {nbors, agnt_pid}}
+  end
+
+  @impl true
+  def terminate(_, {nbors, _}) do
+    IO.puts "Terminating Node #{elem(hd(nbors), 0)}"
   end
 
 end
