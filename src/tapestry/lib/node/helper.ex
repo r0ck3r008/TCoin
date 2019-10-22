8dc8c732ff464067a6436d50f82fc718799c2523
r0ck3r008 <naman.arora@tutanota.com>
2019-10-22 19:09:13 -0400

    limited the hash upto 4 places, implemeted find_best_match and completed publish

--- src/tapestry/lib/node/helper.ex
+++ src/tapestry/lib/node/helper.ex
@@ -4,24 +4,42 @@ defmodule Tapestry.Node.Helper do
     Salty.Hash.Sha256.hash(msg)
     |> elem(1)
     |> Base.encode16()
-    |> String.slice(0, 16)
+    |> String.slice(0, 4)
   end
 
   def remove_deadlocks(_num, _disp_pid, 0), do: :ok
   def remove_deadlocks(num, disp_pid, _dlta), do: remove_deadlocks(num, disp_pid, num-Tapestry.Dispenser.fetch_assigned(disp_pid))
 
-  def publish(nbors, agnt_pid, {hash, msg}) do
-    nbor=find_best_match(nbors, hash)
+  def publish(nbors, agnt_pid, {msg_hash, srvr_pid}) do
+    nbor=find_best_match(nbors, msg_hash)
+    Agent.update(agnt_pid, &(&1++[{msg_hash, srvr_pid}]))
     if elem(nbor, 1)==self() do
-      IO.puts "Publishing #{msg}:#{hash} at #{inspect self()}:#{elem(hd(nbors), 0)}"
-      Agent.update(agnt_pid, &(&1++[{hash, msg}]))
+      IO.puts "Publishing #{msg_hash} at #{inspect self()}:#{elem(hd(nbors), 0)}"
     else
-      send(elem(nbor, 1), {:publish, msg})
+      send(elem(nbor, 1), {:publish, msg_hash, srvr_pid})
     end
   end
 
-  def find_best_match([{self_hash, self_pid} | rest], hash) do
-    #RE match here
+  def find_best_match([{self_hash, self_pid} | rest], msg_hash) do
+    diff=elem(Integer.parse(msg_hash, 16),0)-elem(Integer.parse(self_hash, 16), 0)
+    if diff==1 do
+      {self_hash, self_pid}
+    else
+      #TODO
+      #If this level returns a nil, send to surrogate
+      Enum.at(
+        rest,
+        find_match_lvl(self_hash, msg_hash, 1)
+      )
+    end
+  end
+
+  def find_match_lvl(hash1, hash2, count) do
+    if String.slice(hash1, 0, count)==String.slice(hash2, 0, count) do
+      find_match_lvl(hash1, hash2, count+1)
+    else
+      count
+    end
   end
 
 end
