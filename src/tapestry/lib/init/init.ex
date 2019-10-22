1955226abec5e532151f31f525aff37f1fa36cc5
r0ck3r008 <naman.arora@tutanota.com>
2019-10-22 19:07:46 -0400

    added a call to dolr publish

--- src/tapestry/lib/init/init.ex
+++ src/tapestry/lib/init/init.ex
@@ -7,6 +7,8 @@ defmodule Tapestry.Init do
     #start nodes
     nodes=for _x<-0..n-1, do: Tapestry.Node.start_link
     tasks=for {_, pid}<-nodes, do: Task.async(fn-> task_fn(pid, n, disp_pid, 0) end)
+    :timer.sleep(1000)
+    dolr_call(disp_pid, Enum.map(nodes, fn({:ok, pid})-> pid end))
     #makes main never exit
     for task<-tasks, do: Task.await(task, :infinity)
   end
@@ -19,5 +21,15 @@ defmodule Tapestry.Init do
     :timer.sleep(1000)
     task_fn(pid, n, disp_pid, count)
   end
+
+  def dolr_call(disp_pid, nodes) do
+    nbors_done?(disp_pid, Tapestry.Dispenser.fetch_assigned(disp_pid))
+
+    IO.puts("Publishing!")
+    Tapestry.Dolr.publish(Enum.at(nodes, Salty.Random.uniform(length(nodes))), "YOLO!")
+  end
+
+  def nbors_done?(_disp_pid, 0), do: :ok
+  def nbors_done?(disp_pid, _assigned), do: nbors_done?(disp_pid, Tapestry.Dispenser.fetch_assigned(disp_pid))
 
 end
