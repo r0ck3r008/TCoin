07fe627ecd86c7876c73cd7bdea2789b6df4ec1e
r0ck3r008 <naman.arora@tutanota.com>
2019-10-23 19:56:22 -0400

    added a hop attribute

--- src/tapestry/lib/dolr/dolr.ex
+++ src/tapestry/lib/dolr/dolr.ex
@@ -2,7 +2,7 @@ defmodule Tapestry.Dolr do
 
   def publish(srvr_pid, msg) do
     msg_hash=Tapestry.Node.Helper.hash_it(msg)
-    send(srvr_pid, {:publish, msg_hash, srvr_pid})
+    send(srvr_pid, {:publish, msg_hash, srvr_pid, 0})
   end
 
 end
