a5955c93234d792596ac1936f6d4a779ad395fc3
r0ck3r008 <naman.arora@tutanota.com>
2019-10-23 16:17:21 -0400

    fixed the find_best_match and made the hash upto 3 charachers only

--- src/tapestry/lib/node/helper.ex
+++ src/tapestry/lib/node/helper.ex
@@ -4,7 +4,7 @@ defmodule Tapestry.Node.Helper do
     Salty.Hash.Sha256.hash(msg)
     |> elem(1)
     |> Base.encode16()
-    |> String.slice(0, 4)
+    |> String.slice(0, 3)
   end
 
   def remove_deadlocks(_num, _disp_pid, 0), do: :ok
@@ -27,10 +27,11 @@ defmodule Tapestry.Node.Helper do
     else
       #TODO
       #If this level returns a nil, send to surrogate
-      if is_nil(Enum.at(rest, find_match_lvl(self_hash, msg_hash, 1))) do
-        0
+      nbor=Enum.at(rest, find_match_lvl(self_hash, msg_hash, 0))
+      if is_nil(elem(nbor, 0)) do
+        {self_hash, self_pid}
       else
-        Enum.at(rest, find_match_lvl(self_hash, msg_hash, 1))
+        nbor
       end
     end
   end
