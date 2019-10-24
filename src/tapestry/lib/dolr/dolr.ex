02b017df90eb12b29aacd9f0df4782da509fd3ef
r0ck3r008 <naman.arora@tutanota.com>
2019-10-24 02:23:51 -0400

    implemented route to object

--- src/tapestry/lib/dolr/dolr.ex
+++ src/tapestry/lib/dolr/dolr.ex
@@ -6,8 +6,9 @@ defmodule Tapestry.Dolr do
     send(srvr_pid, {:publish, msg_hash, srvr_pid, 0})
   end
 
-  def route_to_obj(msg) do
-    Tapestry.Node.Helper.hash_it(msg)
+  def route_to_obj(msg, rqstr_pid) do
+    msg_hash=Tapestry.Node.Helper.hash_it(msg)
+    send(rqstr_pid, {:route_o, msg_hash, rqstr_pid})
   end
 
 end
