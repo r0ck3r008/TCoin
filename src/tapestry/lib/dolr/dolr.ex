d22c11c6fad804411208dfe205b0ed28ce222822
r0ck3r008 <naman.arora@tutanota.com>
2019-10-24 01:27:18 -0400

    changed the sequence of arguments to meet the papers requirements, added a store call for storing the object and statred implementing route_to_object

--- src/tapestry/lib/dolr/dolr.ex
+++ src/tapestry/lib/dolr/dolr.ex
@@ -1,8 +1,13 @@
 defmodule Tapestry.Dolr do
 
-  def publish(srvr_pid, msg) do
+  def publish(msg, srvr_pid) do
     msg_hash=Tapestry.Node.Helper.hash_it(msg)
+    send(srvr_pid, {:store, msg_hash, msg})
     send(srvr_pid, {:publish, msg_hash, srvr_pid, 0})
+  end
+
+  def route_to_obj(msg) do
+    Tapestry.Node.Helper.hash_it(msg)
   end
 
 end
