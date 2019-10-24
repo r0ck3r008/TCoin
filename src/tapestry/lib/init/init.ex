2ac3fd7753fe77ca2efa130ecc308daf2b8ef4ee
r0ck3r008 <naman.arora@tutanota.com>
2019-10-24 02:24:31 -0400

    added a call to Tapestry.Dolr.route_to_obj with saperate requester

--- src/tapestry/lib/init/init.ex
+++ src/tapestry/lib/init/init.ex
@@ -24,9 +24,13 @@ defmodule Tapestry.Init do
 
   def dolr_call(disp_pid, nodes) do
     nbors_done?(disp_pid, Tapestry.Dispenser.fetch_assigned(disp_pid))
+    publisher=Enum.at(nodes, Salty.Random.uniform(length(nodes)))
+    rqstr=Enum.at(nodes, Salty.Random.uniform(length(nodes)))
 
     IO.puts("Publishing!")
-    Tapestry.Dolr.publish("HELLO", Enum.at(nodes, Salty.Random.uniform(length(nodes))))
+    Tapestry.Dolr.publish("HELLO", publisher)
+    :timer.sleep(5000)
+    Tapestry.Dolr.route_to_obj("HELLO", rqstr)
   end
 
   def nbors_done?(_disp_pid, 0), do: :ok
