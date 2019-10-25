d5474b27670699fd621bd2f82ca00469d40aee38
r0ck3r008 <naman.arora@tutanota.com>
2019-10-25 07:13:26 -0400

    changed locations of the main calling file and removed old tapestry file

--- src/tapestry/lib/tapestry.ex
+++
@@ -1,7 +0,0 @@
-defmodule Tapestry do
-
-  def start(num) do
-    Tapestry.Init.main(num)
-  end
-
-end
--- src/tapestry/project3.exs
+++ src/tapestry/project3.exs
@@ -2,12 +2,6 @@ args = System.argv()
 numNodes = String.to_integer(Enum.at(args, 0))
 numRequests = String.to_integer(Enum.at(args, 1))
 num_of_args = length(args)
-#IO.puts("length= #{length(args)}")
-failure_percent=
-if num_of_args == 3 do
-  String.to_integer(Enum.at(args, 2))
-else
-  0
-end
-#IO.puts("failure percent= #{failure_percent}")
+failure_percent=  if num_of_args == 3, do: String.to_integer(Enum.at(args, 2)), else: 0
+
 Tapestry.Init.main(numNodes, numRequests, failure_percent)
