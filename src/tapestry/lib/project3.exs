2b7a6a0a3cb4bcf66b7181a4adb1e8f7503ab541
Drona Banerjee <drona95.db@gmail.com>
2019-10-25 07:01:11 +0000

    Provision made for optional failure parameter for bonus. If no value is provided the failure percent is zero.

--- src/tapestry/lib/project3.exs
+++ src/tapestry/lib/project3.exs
@@ -1,4 +1,13 @@
 args = System.argv()
 numNodes = String.to_integer(Enum.at(args, 0))
 numRequests = String.to_integer(Enum.at(args, 1))
-Tapestry.Init.main(numNodes, numRequests)
+num_of_args = length(args)
+#IO.puts("length= #{length(args)}")
+failure_percent=
+if num_of_args == 3 do
+  String.to_integer(Enum.at(args, 2))
+else
+  0
+end
+#IO.puts("failure percent= #{failure_percent}")
+Tapestry.Init.main(numNodes, numRequests, failure_percent)
