07ae371dacde214fda535498a05240376f690d73
r0ck3r008 <naman.arora@tutanota.com>
2019-10-25 21:22:03 -0400

    DOLR API complete with comments

--- src/tapestry/lib/dolr/dolr.ex
+++ src/tapestry/lib/dolr/dolr.ex
@@ -1,11 +1,37 @@
 defmodule Tapestry.Dolr do
 
+  #NOTE
+  #The basic algorithm for publish is:
+  #1. Input what to publish and the piblisher's pid
+  #2. Hash the object and send a publish message with object hash, srvr_pid and hop count
+  #3. The message traverses the network using tapestry routing and tries to find its root node
+  #4. The intermidiatory nodes when receive this message, add this mapping and pass along to nbor
+  #   if already added pass to surrogate
   def publish(msg, srvr_pid) do
     msg_hash=Tapestry.Node.Helper.hash_it(msg)
     GenServer.cast(srvr_pid, {:store, msg_hash, msg})
     send(srvr_pid, {:publish, msg_hash, srvr_pid, 0})
   end
 
+  #NOTE
+  #The basic algorithm for ubpublish is:
+  #1. Input what to unpublish and the publisher's pid
+  #2. Send the unpublish message including initial hop count, message hash to it self for tapestry routing
+  #3. For each intermidiatary node, check if it has mapping, if yes then delete else pass its surrogate
+  #   else pass to its next nbor
+  def unpublish(msg, srvr_pid) do
+    msg_hash=Tapestry.Node.Helper.hash_it(msg)
+    send(srvr_pid, {:unpublish, msg_hash, 0})
+  end
+
+  #NOTE:
+  #The basic algorithm for route to obj is:
+  #1. Inpute the message that need to be looked for and the requestor that need it
+  #2. Send a message to itself requesting the message hash and its own pid along with inital hop count
+  #3. The tapestry algorithm routes it in its own way until it either finds a mapping published when that
+  #   object was published or the root node
+  #4. The root node or the intermidiatary node with object's published mapping sends its server's pid to requestor
+  #   and the requestor can now fetch
   def route_to_obj(msg, rqstr_pid) do
     msg_hash=Tapestry.Node.Helper.hash_it(msg)
     send(rqstr_pid, {:route_o, msg_hash, rqstr_pid, 0})
@@ -21,11 +47,6 @@ defmodule Tapestry.Dolr do
   def route_to_node(src_pid, dest_pid, acc_pid) do
     dest_hash=Tapestry.Node.Helper.hash_it(inspect dest_pid)
     send(src_pid, {:route_n, dest_hash, src_pid, acc_pid, 0})
-  end
-
-  def unpublish(msg, srvr_pid) do
-    msg_hash=Tapestry.Node.Helper.hash_it(msg)
-    send(srvr_pid, {:unpublish, msg_hash, 0})
   end
 
   #NOTE
