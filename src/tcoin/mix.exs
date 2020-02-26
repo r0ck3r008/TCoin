6e4ebc9852c5d33af4c07b67b0f54c8043c3c986
r0ck3r008 <naman.arora@tutanota.com>
2020-02-26 17:03:06 -0500

    update mix.exs

--- src/tcoin/mix.exs
+++ src/tcoin/mix.exs
@@ -5,7 +5,7 @@ defmodule Tcoin.MixProject do
     [
       app: :tcoin,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
